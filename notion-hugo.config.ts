84bcba0896f92d86d01cec1dedc1723b02db56ee
mannilito <60095448+mannilito@users.noreply.github.com>
2024-01-19 10:49:38 +0100

    Update notion-hugo.config.ts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,7 +3,7 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://www.notion.so/mannilito/Interaction-in-Space-dfc1734892e74733a36f1b83dec617b3?pvs=4',
         pages: [
             // {
             //     page_id: '<page_id>',
